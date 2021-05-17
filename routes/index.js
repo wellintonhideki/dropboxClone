5f510a699d0062a4511f18be9ac3cacb25119154
Wellinton Hideki <wellintonhideki@gmail.com>
2021-05-17 14:14:21 -0300

    projectDropboxClone

---
+++ routes/index.js
@@ -0,0 +1,105 @@
+var express = require('express');
+const Formidable = require('formidable');
+var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/file', (req, res) => {
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)){
+
+    fs.readFile(path, (err, data) => {
+
+      if (err){
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      } else {
+
+        res.status(200).end(data);
+
+      }
+
+    });
+    
+  } else {
+
+    res.status(404).json({
+      error:'File not found'
+    });
+  }
+
+});
+
+router.delete('/file', (req, res) => {
+
+  // tratando os arquivos 
+  // chamando formulário
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true 
+  });
+
+  form.parse(req, (err, fields, files) => {
+
+    let path = "./" + fields.path;
+
+    if (fs.existsSync(path)){
+
+      fs.unlink(path, err => {
+
+        if(err){
+
+          res.status(400).json({
+            err
+          });
+        } else {
+
+          res.json({
+            fields
+          });
+
+        }
+
+      });
+      
+    } else {
+
+      res.status(404).json({
+        error:'File not found'
+      });
+
+    }
+
+  });
+
+});
+
+router.post('/upload', (req, res) => {
+
+  // tratando os arquivos 
+  // chamando formulário
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true 
+  });
+
+  form.parse(req, (err, fields, files) => {
+  
+    res.json({
+      files
+    });
+  });
+});
+
+module.exports = router;
